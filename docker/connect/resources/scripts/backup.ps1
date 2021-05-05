38080a690d34d3ad97b7a5bf50989c829b49392e
Robbie Gieze <giezer@eu.objectiflune.com>
2021-05-05 12:42:56 +0200

    Added support for secrets

--- docker/connect/resources/scripts/backup.ps1
+++ docker/connect/resources/scripts/backup.ps1
@@ -1,2 +1,4 @@
 Write-Output "Backing up Connect Server data."
+# Create this folder so we can mount secrets as a subfolder in it, k8s will not mount it if the parent folder doesn't exist.
+New-Item -Path "C:\ProgramData\Objectif Lune\OL Connect" -Name "Initialization" -ItemType "directory"
 Copy-Item -Path "C:\ProgramData\Objectif Lune\OL Connect" -Destination "C:\Connect\Backup\Connect Server\ProgramData" -Recurse -Force
