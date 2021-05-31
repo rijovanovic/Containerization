a57baa0c35f2005579b371da5e2ed92677b7cc71
giezer <giezer@eu.objectiflune.com>
2021-05-31 04:30:20 -0700

    Make sure passwords never expire, removed password script from mysql container as it is not used.

--- docker/connect/resources/scripts/set_password.ps1
+++ docker/connect/resources/scripts/set_password.ps1
@@ -1,4 +1,4 @@
 $password = ConvertTo-SecureString -String "UPLuxSk5B1A7P1eltoDA" -AsPlainText -Force
-Get-LocalUser -Name "ConnectAdmin" | Set-LocalUser -Password $password
+Get-LocalUser -Name "ConnectAdmin" | Set-LocalUser -Password $password -PasswordNeverExpires $true
 
 
--- docker/mysql/resources/scripts/set_password.ps1
+++
@@ -1,4 +0,0 @@
-$password = ConvertTo-SecureString -String "Objlune!1" -AsPlainText -Force
-Get-LocalUser -Name "ConnectAdmin" | Set-LocalUser -Password $password
-
-
