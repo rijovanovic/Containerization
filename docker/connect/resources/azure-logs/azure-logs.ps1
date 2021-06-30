5a641008d7faefb2b8bf2344405e7e46a20e33f9
giezer <giezer@eu.objectiflune.com>
2021-06-30 16:22:15 +0200

    SHARED-82373 Azure Logs
    Added the missing registry key with the performance counters into the container. This seems to fix all the errors that we got before.

---
+++ docker/connect/resources/azure-logs/azure-logs.ps1
@@ -0,0 +1,17 @@
+Write-Output "Changing ini files to add Azure Logging..."
+
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Datamapper Engine\DatamapperEngine.ini" -Value "-javaagent:C:\applicationinsights-agent-3.1.0.jar"
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Datamapper Engine\DatamapperEngine.ini" -Value "-Dapplicationinsights.configuration.file=dm-app-insights.json"
+
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Merge Engine\MergeEngine.ini" -Value "-javaagent:C:\applicationinsights-agent-3.1.0.jar"
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Merge Engine\MergeEngine.ini" -Value "-Dapplicationinsights.configuration.file=me-app-insights.json"
+
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Server\Server.ini" -Value "-javaagent:C:\applicationinsights-agent-3.1.0.jar"
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Server\Server.ini" -Value "-Dapplicationinsights.configuration.file=se-app-insights.json"
+
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Weaver Engine\WeaverEngine.ini" -Value "-javaagent:C:\applicationinsights-agent-3.1.0.jar"
+Add-Content -Path "C:\Program Files\Objectif Lune\OL Connect\Connect Weaver Engine\WeaverEngine.ini" -Value "-Dapplicationinsights.configuration.file=we-app-insights.json"
+
+# Azure Application Insights jar needs a registry key for the performance counters
+# For some reason this key is not present in a windows container, we fix that by importing the values
+reg.exe import "C:\connect\azure-logs\perflib009.reg"
