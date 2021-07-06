612c84949952a4ca0d3e8ea189a6589b30b3e80c
giezer <giezer@eu.objectiflune.com>
2021-07-06 10:37:57 +0200

    SHARED-82748 Create Alpha OL Container for R&D and QA
    Moved image related files into image subfolder. This will reduce the image size as the iso file is not in the image anymore.
    
    Fixed an issue in the build script that did not delete all setup files from a previous run. Increased sleep to allow extraction of setup files to fully complete.

--- docker/connect/build-image-from-iso.ps1
+++ docker/connect/build-image-from-iso.ps1
@@ -5,7 +5,7 @@ $dockerImageName = "connect:latest"
 # Don't forget to change the name of the docker image in the variable at the top of this file
 
 # Delete old setup files
-Get-ChildItem -Path "resources\setup" | Remove-Item -Recurse -Force -Exclude "install.bat", "install.properties", ".gitignore"
+Get-ChildItem -Path "image\resources\setup" -Recurse | Remove-Item -Recurse -Force -Exclude "install.bat", "install.properties", ".gitignore"
 
 # Mount iso 
 $iso = (Get-ChildItem *.iso)[0].Name
@@ -20,15 +20,15 @@ $sourcePath = $driveLetter + ":\*"
 
 # Copy files from iso to setup folder
 Write-Output "Copy files from ISO to setup folder..."
-$destination = (Get-Location).Path + "\resources\setup"
+$destination = (Get-Location).Path + "\image\resources\setup"
 Copy-Item -Path $sourcePath -Destination $destination -Recurse -Force
 
 # Dismount iso
 Dismount-DiskImage -ImagePath $isoPath
 
 # Extract setup files
-.\resources\setup\PReS_Connect_Setup_x86_64.exe -nr -gm2 '-InstallPath=".\\resources\\setup"'
+.\image\resources\setup\PReS_Connect_Setup_x86_64.exe -nr -gm2 '-InstallPath=".\\image\\resources\\setup"'
 # Command above returns immediately but extraction is still going on in background so we have to sleep here
-Start-Sleep -s 5
+Start-Sleep -s 30
 
-docker build -t $dockerImageName .
+docker build -t $dockerImageName .\image
